b3ac41d2ed4975e0ffa7a00daa99b355e1c62ea1
Anchal kumari <anchalkum2630@gmail.com>
2024-07-11 19:45:53 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+import Chai from './Chai'
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>happy to learn react</h1>
+    </>
+  );
+}
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>I love my family so much!</h3>
+    )
+}
+export default Chai;
